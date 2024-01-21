395782bf6c9afa2bb2f2ff6b630d994b494bd174
im_remond <okeraymondjesutofunmi99@gmail.com>
2024-01-21 14:16:55 -0800

    first commit

--- client/domicile/src/pages/rentPage/Mobile/MobileRentPage.js
+++ client/domicile/src/pages/rentPage/Mobile/MobileRentPage.js
@@ -18,7 +18,53 @@ const MobileRentPage = () => {
             </div>
           </div>
 
-          <div className='w-[90%] h-[77%] flex flex-col flex-wrap gap-4'>
+          <div className='w-[90%] h-[85%] flex flex-col flex-wrap gap-4'>
+            <Link to={'/sell'} className='w-[48%] h-[40svh]  rounded-2xl bg-white border border-[#091A2B] shadow-black shadow-lg'>
+              <div className='w-full h-[60%] '>
+                <img src={Image1} alt='img' className='object-fill w-full h-full' />
+              </div>
+              <div className='w-full h-[40%]  px-2'>
+                <div className=' w-full h-[40%]'>
+                  <h3 className=' text-[calc(1px_+_1.2svw_+_1.2svh)] font-poppins font-[800] capitalize'>
+                    2 bedroom flat
+                  </h3>
+                </div>
+                <div className=' w-full h-[30%] flex items-center'>
+                  <h3 className=' text-[calc(1px_+_1svw_+_1svh)] font-poppins font-[400]'>
+                    300000
+                  </h3>
+                </div>
+                <div className=' w-full h-[30%] flex items-center'>
+                  <BsGeoAltFill className='fill-[grey] text-[calc(1px_+_1.5svw_+_1.5svh)]' />
+                  <h3 className=' text-[calc(1px_+_1svw_+_1svh)]'>
+                    saapade
+                  </h3>
+                </div>
+              </div>
+            </Link>
+            <Link to={'/sell'} className='w-[48%] h-[40svh]  rounded-2xl bg-white border border-[#091A2B] shadow-black shadow-lg'>
+              <div className='w-full h-[60%] '>
+                <img src={Image1} alt='img' className='object-fill w-full h-full' />
+              </div>
+              <div className='w-full h-[40%]  px-2'>
+                <div className=' w-full h-[40%]'>
+                  <h3 className=' text-[calc(1px_+_1.2svw_+_1.2svh)] font-poppins font-[800] capitalize'>
+                    2 bedroom flat
+                  </h3>
+                </div>
+                <div className=' w-full h-[30%] flex items-center'>
+                  <h3 className=' text-[calc(1px_+_1svw_+_1svh)] font-poppins font-[400]'>
+                    300000
+                  </h3>
+                </div>
+                <div className=' w-full h-[30%] flex items-center'>
+                  <BsGeoAltFill className='fill-[grey] text-[calc(1px_+_1.5svw_+_1.5svh)]' />
+                  <h3 className=' text-[calc(1px_+_1svw_+_1svh)]'>
+                    saapade
+                  </h3>
+                </div>
+              </div>
+            </Link>
             <Link to={'/sell'} className='w-[48%] h-[40svh]  rounded-2xl bg-white border border-[#091A2B] shadow-black shadow-lg'>
               <div className='w-full h-[60%] '>
                 <img src={Image1} alt='img' className='object-fill w-full h-full' />
